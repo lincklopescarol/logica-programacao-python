167be4454ba6f59dd8ef3e6795cf7d33112f8fa9
lincklopescarol <lincklopescarol@gmail.com>
2025-01-22 17:22:23 -0300

    feat: adicionando finalização do exercício da aula_06

--- aula_06/cadastro_dados_com_dicionario.py
+++ aula_06/cadastro_dados_com_dicionario.py
@@ -4,19 +4,41 @@ while True:
     terminar_cadastro = input('Deseja cadastrar uma pessoa? [S/N]')
     if terminar_cadastro.upper() in 'N':
         break
-    if terminar_cadastro.upper() not in 'S':
-        print('Digite S para Sim ou N para Não.')
-        break
 
     nome = input('Qual o nome? ')
-    cidade = input('Qual a cidade? ')
-    ano = int(input('Qual o ano de nascimento? '))
+    cidade = input('Qual sua cidade? ')
+    ano = int(input('Qual seu ano de nascimento? '))
 
     cadastro['nome'].append(nome)
     cadastro['cidade'].append(cidade)
     cadastro['ano'].append(ano)
 
-print(cadastro)
+lista_idades = []
+lista_idades_menor_que_trinta = []
+lista_idades_acima_media = []
+
+for ano in cadastro['ano']:
+    idade = (2025 - ano)
+    lista_idades.append(idade)
+
+numero_cadastros = len(cadastro['nome'])
+soma_idades = sum(lista_idades)
+media_idades = soma_idades // numero_cadastros
+
+for idade, nome in zip(lista_idades, cadastro['nome']):
+    if idade < 30:
+        lista_idades_menor_que_trinta.append(nome)
+
+    if idade > media_idades:
+        lista_idades_acima_media.append(nome)
+
+print('Os cadastros: ', cadastro)
+print('O número de cadastros: ', numero_cadastros)
+print('A média das idades é: ', media_idades)
+print('A lista de pessoas com menos de 30 anos é: ', lista_idades_menor_que_trinta)
+print('A lista de pessoas com idade acima da média é: ', lista_idades_acima_media)
+
+
 
 
 
